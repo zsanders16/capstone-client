75285cb32ba386b88fa5eaf31ddceead92b84e98
Zachary Sanders <zsanders@icloud.com>
2017-07-01 16:25:36 -0600

    connected navbar redux state

---
+++ src/components/Compare.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Segment } from 'semantic-ui-react';
+
+class Compare extends React.Component{
+  render(){
+    return(
+      <Segment basic>
+        <h1>This is the Compare Segment</h1>
+      </Segment>
+    )
+  }
+}
+
+export default Compare;
--- src/components/WeatherStation.js
+++ src/components/WeatherStation.js
@@ -1,23 +1,40 @@
-import React, { Component } from 'react'
-import { Grid, Segment } from 'semantic-ui-react'
-import CurrentConditions from './CurrentConditions'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Grid, Segment } from 'semantic-ui-react';
+import CurrentConditions from './CurrentConditions';
+import Compare from './Compare';
 
 class WeatherStation extends Component {
 
+  showHome = () => (
+    <Grid.Row columns={16}>
+      <Grid.Column width={12} className='ws_area'>
+        <CurrentConditions />
+      </Grid.Column>
+      <Grid.Column width={4} className='ws_area'>
+        <Segment>
+          Command Central
+        </Segment>
+      </Grid.Column>
+    </Grid.Row>
+  )
+
   render(){
-    return (
-      <Grid.Row columns={16}>
-        <Grid.Column width={12} className='ws_area'>
-          <CurrentConditions />
-        </Grid.Column>
-        <Grid.Column width={4} className='ws_area'>
-          <Segment>
-            Command Central
-          </Segment>
-        </Grid.Column>
-      </Grid.Row>
-    )
+    let { navbarItem } = this.props
+    switch (navbarItem) {
+        case 'home':
+          return this.showHome();
+        case 'historical':
+          return <Compare />
+        default:
+          return <h1>Hello</h1>
+    }
+
   }
 }
 
-export default WeatherStation
+const mapStateToProps = (state) => {
+  return {navbarItem: state.navbar};
+}
+
+export default connect(mapStateToProps)(WeatherStation);
