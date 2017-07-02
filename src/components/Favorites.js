1cdaaa05a4e29950c457044b1e0d580e78aa47f4
IMS-Global <brennick.sci@gmail.com>
2017-07-01 21:46:57 -0600

    Changes to the Favorites Reducer, Action, Etc. to resolve naming conflicts with Javascript.

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,23 +1,71 @@
 import React, { Component } from 'react'
-import { Segment } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { Grid, Segment, Message, Button, Divider } from 'semantic-ui-react'
+import Favorite from './Favorite'
+import { favorites } from '../actions/favorites'
 
 class Favorites extends Component {
-  state = {
-    location: {
-      city: null,
-      state: null,
-      country: null,
-    },
-    preferences: {
-      settings: {}
+  state = { favorites: [] }
+
+  componentDidMount = () => {
+    let { dispatch } = this.props
+    // Set initial Favorites
+    dispatch(favorites())
+    // Place locations in local state
+    if( this.props.favorites ) {
+      this.setState({ ...this.props.favorites
+      })
+    }
+  }
+
+  displayFavorites = () => {
+    let { favorites } = this.state
+    if( favorites.length > 0 ){
+      return favorites.map( ( loc ) => {
+        return ( <Favorite {...fav} /> )
+      })
+    } else {
+      return (
+        <Message>
+          <Message.Header>
+            Favorites Not Set
+          </Message.Header>
+          <Divider />
+          <p>
+            Please set a favorite<br />using the buttons below.
+          </p>
+        </Message>
+      )
     }
   }
 
   render(){
     return (
-
+      <Grid>
+        <Grid.Row>
+          <Grid.Column>
+            <Segment className='fav-scroll'>
+              { this.displayFavorites() }
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <Button.Group size='mini'>
+              <Button icon='add' />
+              <Button icon='remove' />
+              <Button icon='edit' />
+            </Button.Group>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     )
   }
 }
 
-export default Favorites
+const mapStateToProps = ( state ) => {
+  // TODO: this is just a prototype
+  return { favorites: state.favorites }
+}
+
+export default connect(mapStateToProps)(Favorites)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import navbar from './navbar';
+import favorites from './favorites'
 
 const rootReducer = combineReducers({
   navbar,
+  favorites,
 })
 
 export default rootReducer;
