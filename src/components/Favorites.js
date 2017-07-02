7cb0911abefdfc07c792c1619bbda6168f37724f
IMS-Global <brennick.sci@gmail.com>
2017-07-01 18:06:33 -0600

    Aligning of Navbar with the rest of the view with Grids. Initial commit for empty Favorites Element.

---
+++ src/components/Favorites.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Segment } from 'semantic-ui-react'
+
+class Favorites extends Component {
+  state = {
+    location: {
+      city: null,
+      state: null,
+      country: null,
+    },
+    preferences: {
+      settings: {}
+    }
+  }
+
+  render(){
+    return (
+
+    )
+  }
+}
+
+export default Favorites
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Menu, Input, Dropdown } from 'semantic-ui-react';
+import { Grid, Menu, Input, Dropdown } from 'semantic-ui-react';
 import { setNavbar} from '../actions/navbar';
 import { connect } from 'react-redux';
 
@@ -18,27 +18,31 @@ class NavBar extends Component {
   render(){
     const { activeItem } = this.state
     return (
-      <Menu pointing fluid >
-        <Menu.Item header>Weather Station</Menu.Item>
-        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-        <Menu.Item name='historical' active={activeItem === 'historical'} onClick={this.handleItemClick} />
-        <Menu.Item name='local' active={activeItem === 'local'} onClick={this.handleItemClick} />
-          <Dropdown item text='Maps'>
-            <Dropdown.Menu>
-              <Dropdown.Item name='barometric' onClick={this.handleItemClick}>Barometric</Dropdown.Item>
-              <Dropdown.Item name='radar' onClick={this.handleItemClick}>Radar</Dropdown.Item>
-              <Dropdown.Item name='satallite' onClick={this.handleItemClick}>Regional Satallite</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        <Menu.Item position='right' className='nb-border-right'>
-          <Input
-            action={{ type: 'submit', content: 'Go' }}
-            placeholder='Search...'
-            onSubmit={this.handleSearch} />
-        </Menu.Item>
-        <Menu.Item name='settings' as={ Link } to='/settings' />
-        <Menu.Item name='login' as={ Link } to='/auth' />
-      </Menu>
+      <Grid.Row columns={16}>
+        <Grid.Column width={16}>
+          <Menu pointing fluid >
+            <Menu.Item header>Weather Station</Menu.Item>
+            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+            <Menu.Item name='historical' active={activeItem === 'historical'} onClick={this.handleItemClick} />
+            <Menu.Item name='local' active={activeItem === 'local'} onClick={this.handleItemClick} />
+              <Dropdown item text='Maps'>
+                <Dropdown.Menu>
+                  <Dropdown.Item name='barometric' onClick={this.handleItemClick}>Barometric</Dropdown.Item>
+                  <Dropdown.Item name='radar' onClick={this.handleItemClick}>Radar</Dropdown.Item>
+                  <Dropdown.Item name='satallite' onClick={this.handleItemClick}>Regional Satallite</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            <Menu.Item position='right' className='nb-border-right'>
+              <Input
+                action={{ type: 'submit', content: 'Go' }}
+                placeholder='Search...'
+                onSubmit={this.handleSearch} />
+            </Menu.Item>
+            <Menu.Item name='settings' as={ Link } to='/settings' />
+            <Menu.Item name='login' as={ Link } to='/auth' />
+          </Menu>
+        </Grid.Column>
+      </Grid.Row>
     )
   }
 }
